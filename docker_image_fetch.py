6510932768178a10ab60eeaeb354ff3733172f41
soufaker <44189635+Soufaker@users.noreply.github.com>
2023-05-17 21:02:38 +0800

    Create docker_image_fetch.py

---
+++ docker_image_fetch.py
@@ -0,0 +1,98 @@
+import os
+import json
+import optparse
+import requests
+
+# pulls Docker Images from unauthenticated docker registry api.
+# and checks for docker misconfigurations.
+
+apiversion = "v2"
+final_list_of_blobs = []
+
+# Disable insecure request warning
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+parser = optparse.OptionParser()
+parser.add_option('-u', '--url', action="store", dest="url",
+                  help="URL Endpoint for Docker Registry API v2. Eg https://IP:Port", default="spam")
+options, args = parser.parse_args()
+url = options.url
+
+
+def list_repos():
+    req = requests.get(url + "/" + apiversion + "/_catalog", verify=False)
+    return json.loads(req.text)["repositories"]
+
+
+def find_tags(reponame):
+    req = requests.get(url + "/" + apiversion + "/" + reponame + "/tags/list", verify=False)
+    print("\n")
+    data = json.loads(req.content)
+    if "tags" in data:
+        return data["tags"]
+
+
+def list_blobs(reponame, tag):
+    req = requests.get(url + "/" + apiversion + "/" + reponame + "/manifests/" + tag, verify=False)
+    data = json.loads(req.content)
+    if "fsLayers" in data:
+        for x in data["fsLayers"]:
+            curr_blob = x['blobSum'].split(":")[1]
+            if curr_blob not in final_list_of_blobs:
+                final_list_of_blobs.append(curr_blob)
+
+
+def download_blobs(reponame, blobdigest, dirname):
+    req = requests.get(url + "/" + apiversion + "/" + reponame + "/blobs/sha256:" + blobdigest, verify=False)
+    filename = "%s.tar.gz" % blobdigest
+    with open(dirname + "/" + filename, 'wb') as test:
+        test.write(req.content)
+
+
+def main():
+    if url is not "spam":
+        list_of_repos = list_repos()
+        print('[+] List of Repositories:'+'\n')
+        for x in list_of_repos:
+            print(x)
+        target_repo = input("\nWhich repo would you like to download?:  ")
+        if target_repo in list_of_repos:
+            tags = find_tags(target_repo)
+            if tags is not None:
+                print('[+] Available Tags:'+'\n')
+                for x in tags:
+                    print(x)
+                target_tag = input("\nWhich tag would you like to download?:  ")
+                if target_tag in tags:
+                    list_blobs(target_repo, target_tag)
+                    dir_cur = os.getcwd()
+                    print(dir_cur)
+                    if '/' in target_repo:
+                        temp_l = target_repo.split('/')
+                        temp_dir = ''
+                        for i in temp_l:
+                            temp_dir = temp_dir + '_' + i
+                        dir = dir_cur +'\\'+ temp_dir
+                    else:
+                        dir = dir_cur + '\\' + target_repo
+                    print('11111')
+                    print(dir)
+                    os.mkdir(dir)
+                    print("Now sit back and relax. I will download all the blobs for you in"+str(dir)+"directory. \nOpen the directory, unzip all the files and explore like a Boss. ")
+                    for x in final_list_of_blobs:
+                        print("\n[+] Downloading Blob: "+x)
+                        download_blobs(target_repo, x, dir)
+                else:
+                    print("No such Tag Available. Qutting....")
+            else:
+                print("[+] No Tags Available. Quitting....")
+        else:
+            print("No such repo found. Quitting....")
+    else:
+        print("\n[-] Please use -u option to define API Endpoint, e.g. https://IP:Port\n")
+
+
+if __name__ == "__main__":
+    main()
